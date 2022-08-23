cf2c700a2d4f1e4176ea606434ed280bd73dd869
unknown <malatesh.xworkz@gmail.com>
2022-08-23 10:53:56 -0700

    Hierarchical Inheritance

---
+++ automobile/src/com/xworkz/automobile/art/AutomobileArt.java
@@ -0,0 +1,35 @@
+package com.xworkz.automobile.art;
+
+import com.xworkz.automobile.base.Automobile;
+import com.xworkz.automobile.sub.HondaAutomobile;
+import com.xworkz.automobile.sub1.TataAutomobile;
+
+public class AutomobileArt {
+
+	public static void main(String[] args) {
+
+		Automobile automobile = new HondaAutomobile(9.15,6.15);
+		double am=automobile.sellAccessories("break cable");
+		System.out.println(am);
+		automobile.setType("two wheeler");
+		automobile.setBrand("Hero");
+		automobile.torisu();
+
+		System.out.println("<==================================>");
+
+		HondaAutomobile honda = new HondaAutomobile(9.35, 6.35);
+		honda.setType("Four wheeler");
+		honda.setBrand("Honda");
+		honda.torisu();
+
+		System.out.println("<==================================>");
+
+		TataAutomobile tata = new TataAutomobile();
+		tata.setType("Four wheeler");
+		tata.setBrand("Tata");
+		tata.torisu();
+
+		System.out.println("<==================================>");
+	}
+
+}
